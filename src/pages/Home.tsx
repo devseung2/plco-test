5c2cb5cb97e7bcc4ef296d2243ae2a8c0e620ff9
dev_seung2 <dev_seung2@naver.com>
2022-12-09 21:11:35 +0900

    update: player 컴포넌트 추가로 인한 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
-import React from "react"
-/** @jsxImportSource @emotion/react */
-import { jsx, css } from "@emotion/react"
+import React from 'react'
+import Player from '../components/Player'
 
 const Home = () => {
-  return <div>Home</div>
+  return <Player />
 }
 
 export default Home
