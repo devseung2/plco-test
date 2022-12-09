27490d806bd01e8b4b7136908f7a3bb33b7c865f
dev_seung2 <dev_seung2@naver.com>
2022-12-09 23:47:22 +0900

    add: types 파일 추가

---
+++ src/constants/types.ts
@@ -0,0 +1,16 @@
+export type playerListInfoTypes = {
+  acwr: number
+  user: {
+    id: number
+    pictureUrl?: string | null
+    name: string
+    position: string
+  }
+}
+
+export type pageInfoTypes = {
+  limit: number
+  hasNext: boolean
+  hasPrev: boolean
+  pageNum: number
+}
