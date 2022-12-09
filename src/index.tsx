fc25518e922910df90432e5f0eb2cbaa52d4c971
dev_seung2 <dev_seung2@naver.com>
2022-12-09 20:59:52 +0900

    update: reset css 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { Global } from "@emotion/react"
+import reset from "./reset"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 )
---
+++ src/reset.ts
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+const reset = css({
+  "*": {
+    boxSizing: "border-box",
+  },
+  "html, body": {
+    width: "100%",
+    height: "100%",
+    margin: "0",
+    padding: "0",
+    fontFamily: "Noto Sans KR",
+    fontSize: "16px",
+    color: "black",
+  },
+  "p, ul, li ": {
+    margin: "0",
+    padding: "0",
+  },
+  "li ": {
+    listStyle: "none",
+  },
+})
+
+export default reset
