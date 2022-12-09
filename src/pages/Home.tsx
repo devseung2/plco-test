e16be5c72e299939f5f1366516c6b5f33ada0771
dev_seung2 <dev_seung2@naver.com>
2022-12-10 00:01:41 +0900

    update: 홈 관련 페이지 스타일 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import Player from '../components/Player'
 
 const Home = () => {
-  return <Player />
+  return (
+    <div css={homeContainer}>
+      <Player />
+    </div>
+  )
 }
 
 export default Home
+
+const homeContainer = css({
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  minWidth: '400px',
+  minHeight: '600px',
+})
