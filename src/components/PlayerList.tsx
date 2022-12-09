27641d8f4b7435f936bc1d9fdba3b24a7ec582d0
dev_seung2 <dev_seung2@naver.com>
2022-12-10 00:01:21 +0900

    update: 미사용 코드 삭제

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { playerListInfoTypes } from '../constants/types'
