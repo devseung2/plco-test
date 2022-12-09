5976d4b3fdf5cc943207f9702db0ef80e2e38bf0
dev_seung2 <dev_seung2@naver.com>
2022-12-09 20:45:47 +0900

    add: home 페이지 파일 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+/** @jsxImportSource @emotion/react */
+import { jsx, css } from "@emotion/react"
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
