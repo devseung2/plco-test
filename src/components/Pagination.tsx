5c21604b894205185860a14cdce1fc737bd95faa
dev_seung2 <dev_seung2@naver.com>
2022-12-10 00:34:25 +0900

    feat: 플레이어 리스트 조회

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+import { pageInfoTypes } from '../constants/types'
+
+import leftBtnIcons from '../assets/icons/left-arrow.svg'
+import leftBtnActiveIcons from '../assets/icons/left-arrow-active.svg'
+import rightBtnIcons from '../assets/icons/right-arrow.svg'
+import rightBtnActiveIcons from '../assets/icons/right-arrow-active.svg'
+
+type PropsTypes = {
+  pageInfo: pageInfoTypes
+  movePage: (pageNum: number) => void
+}
+
+const Pagination = ({ pageInfo, movePage }: PropsTypes) => {
+  return (
+    <div css={pageSelectWrap}>
+      <button css={[pageMoveBtn, prevBtn, pageInfo.hasPrev && activePageMoveBtn]} onClick={() => pageInfo.hasPrev && movePage(-1)}>
+        <img src={pageInfo.hasPrev ? leftBtnActiveIcons : leftBtnIcons} alt="left move btn" />
+      </button>
+      <button css={[pageMoveBtn, nextBtn, pageInfo.hasNext && activePageMoveBtn]} onClick={() => pageInfo.hasNext && movePage(1)}>
+        <img src={pageInfo.hasNext ? rightBtnActiveIcons : rightBtnIcons} alt="right move btn" />
+      </button>
+    </div>
+  )
+}
+
+export default Pagination
+
+const pageSelectWrap = css({
+  display: 'flex',
+  alignItems: 'center',
+  position: 'absolute',
+  right: '0',
+  width: '100px',
+  height: '32px',
+  marginTop: '16px',
+  marginRight: '16px',
+})
+
+const pageMoveBtn = css({
+  color: '#C5CEE0',
+  border: 'none',
+  background: 'transparent',
+  padding: 0,
+})
+
+const pageMoveBtnPosition = 12.42
+
+const prevBtn = css({
+  position: 'absolute',
+  left: `${pageMoveBtnPosition}px`,
+})
+
+const nextBtn = css({
+  position: 'absolute',
+  right: `${pageMoveBtnPosition}px`,
+})
+
+const activePageMoveBtn = css({
+  color: '#222B45',
+  cursor: 'pointer',
+})
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
+import PlayerList from './PlayerList'
+import Pagination from './Pagination'
+import { playerListInfoTypes, pageInfoTypes } from '../constants/types'
+import DEFAULT_DATA from '../player.json'
+
+const DEFAULT_PAGE_INFO = {
+  limit: 5,
+  pageNum: 0,
+  hasNext: false,
+  hasPrev: false,
+  acwr: 1.5,
+}
+
 const Player = () => {
+  const [playerListInfo, setPlayerListInfo] = useState<playerListInfoTypes[]>([])
+  const [playerTotal, setPlayerTotal] = useState(0)
+  const [pageInfo, setPageInfo] = useState<pageInfoTypes>(Object.assign({}, DEFAULT_PAGE_INFO))
+  const [playerList, setPlayerList] = useState<playerListInfoTypes[]>([])
+
+  useEffect(() => {
+    const { limit, entries } = DEFAULT_DATA
+
+    const entriesByDesc = entries.filter((playerInfo) => playerInfo.acwr >= pageInfo.acwr).sort((a, b) => b.acwr - a.acwr)
+
+    setPlayerListInfo(entriesByDesc)
+    setPlayerTotal(entriesByDesc.length)
+    setPageInfo((prev) => ({ ...prev, limit, pageNum: 1 }))
+  }, [])
+
+  useEffect(() => {
+    const searchRange = (pageInfo.pageNum - 1) * pageInfo.limit
+
+    setPlayerList(playerListInfo.slice(searchRange, searchRange + pageInfo.limit))
+    setPageBtn()
+  }, [pageInfo.pageNum])
+
+  const setPageBtn = () => {
+    let _hasNext = false
+    let _hasPrev = false
+
+    if (pageInfo.pageNum < getLastPageNum()) _hasNext = true
+    if (pageInfo.pageNum > 1) _hasPrev = true
+
+    setPageInfo((prev) => ({ ...prev, hasPrev: _hasPrev, hasNext: _hasNext }))
+  }
+
+  const movePage = (operation: number) => setPageInfo({ ...pageInfo, pageNum: pageInfo.pageNum + operation })
+  const getLastPageNum = () => Math.ceil(playerTotal / pageInfo.limit)
+
   return (
     <div css={playerContainer}>
-      <div css={title}>😱 부상위험도 높은 선수 (20명)</div>
-      <div css={subTitle}>기준 ・ 부상위험도(ACWR) 1.5 이상</div>
+      <div css={mainWrap}>
+        <div css={title}>😱 부상위험도 높은 선수 ({playerTotal}명)</div>
+        <div css={subTitle}>기준 ・ 부상위험도(ACWR) {pageInfo.acwr} 이상</div>
+        {playerList.length > 0 && <PlayerList playerList={playerList} limit={pageInfo.limit} />}
+        {playerTotal && (
+          <div css={paginationNumWrap}>
+            <span className="active">{pageInfo.pageNum}</span>
+            <span className="crossLine">/</span>
+            <span>{getLastPageNum()}</span>
+          </div>
+        )}
+      </div>
+      <div css={divider} />
+      <Pagination pageInfo={pageInfo} movePage={movePage} />
     </div>
   )
 }
@@ -14,13 +74,17 @@ const Player = () => {
 export default Player
 
 const playerContainer = css({
+  position: 'relative',
   width: '343px',
   minHeight: '500px',
-  padding: '16px',
   backgroundColor: '#FFFFFF',
   border: '1px solid #E4E9F2',
   boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
   borderRadius: '8px',
+})
+
+const mainWrap = css({
+  padding: '16px',
 })
 
 const title = css({
@@ -37,4 +101,31 @@ const subTitle = css({
   fontSize: '12px',
   lineHeight: '18px',
   color: '#8F9BB3',
+})
+
+const paginationNumWrap = css({
+  display: 'flex',
+  justifyContent: 'center',
+  width: '100%',
+  fontWeight: 700,
+  fontSize: '12px',
+  lineHeight: '18px',
+
+  span: {
+    color: '#C5CEE0',
+  },
+
+  'span.crossLine': {
+    margin: '0 4px',
+  },
+
+  'span.active': {
+    color: '#222B45',
+  },
+})
+
+const divider = css({
+  width: '100%',
+  height: '1px',
+  backgroundColor: '#EDF1F7',
 })
