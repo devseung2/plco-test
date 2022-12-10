356f0d12e9103e49f948b3dc9ee22f3f51fe7b3f
dev_seung2 <dev_seung2@naver.com>
2022-12-10 18:40:15 +0900

    update: prettier 적용

--- src/reset.ts
+++ src/reset.ts
@@ -1,25 +1,25 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"
+import { css } from '@emotion/react'
 
 const reset = css({
-  "*": {
-    boxSizing: "border-box",
+  '*': {
+    boxSizing: 'border-box',
   },
-  "html, body": {
-    width: "100%",
-    height: "100%",
-    margin: "0",
-    padding: "0",
-    fontFamily: "Noto Sans KR",
-    fontSize: "16px",
-    color: "black",
+  'html, body': {
+    width: '100%',
+    height: '100%',
+    margin: '0',
+    padding: '0',
+    fontFamily: 'Noto Sans KR',
+    fontSize: '16px',
+    color: 'black',
   },
-  "p, ul, li ": {
-    margin: "0",
-    padding: "0",
+  'p, ul, li ': {
+    margin: '0',
+    padding: '0',
   },
-  "li ": {
-    listStyle: "none",
+  'li ': {
+    listStyle: 'none',
   },
 })
 
